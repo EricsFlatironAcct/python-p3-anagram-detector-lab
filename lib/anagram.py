e531d8d91c0ae96b3b7edbc1d4e0901a87f3be12
Eric Krukowski <erickruk@amazon.com>
2023-07-05 02:34:29 -0400

    Lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+class Anagram:
+   def __init__(self, word):
+      self.set_word(word)
+
+   def set_word(self, word):
+      self._word = word
+   
+   def get_word(self):
+      return self._word
+
+   def match(self, matchable):
+      matches = []
+      for m in matchable:
+         if sorted(self.get_word())== sorted(m):
+            matches.append(m)
+      return matches
+
+   word = property(get_word, set_word)
